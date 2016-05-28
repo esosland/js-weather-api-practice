ba07330af071abf9b262f8a29d6f4c1983110193
Elissa Sosland <esosland@gmail.com>
2016-05-27 18:20:11 -0700

    create weather API app for javascript practice

---
+++ js/weather-interface.js
@@ -0,0 +1,10 @@
+var Weather = require('./../js/weather.js').Weather;
+
+$(document).ready(function() {
+  var currentWeatherObject = new Weather();
+  $('#weatherLocation').click(function() {
+    var city = $('#location').val();
+    $('#location').val("");
+    currentWeatherObject.getWeather(city);
+  });
+});
